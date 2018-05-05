94cb126e234163cf3928a9a06e73781a8d4b0b6e
Zythiku <zythiku@gmail.com>
2018-05-05 13:42:39 -0700

    Adjust beat timing Add beat ring ui

---
+++ Assets/Code/BeatTrackerUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatTrackerUI : MonoBehaviour
+{
+    [SerializeField]
+    PlayerFireScript m_playerFireScript;
+
+    [SerializeField]
+    Image m_ring;
+
+    [SerializeField]
+    Image m_inner;
+
+    [SerializeField]
+    float m_startRingScale = 3f;
+
+    [SerializeField]
+    float m_endRingScale = 1f;
+
+    [SerializeField]
+    Color m_startRingColor = Color.clear;
+
+    [SerializeField]
+    Color m_endRingColor = Color.green;
+       
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_ring.transform.localScale = Vector3.one *
+            Mathf.Lerp(m_startRingScale, m_endRingScale, m_playerFireScript.ToBeatCenterPercentage);
+        m_ring.color = Color.Lerp(m_startRingColor, m_endRingColor, m_playerFireScript.ToBeatCenterPercentage);
+	}
+
+    public void OnFire()
+    {
+        StartCoroutine(OnFireRoutine());
+    }
+
+    private IEnumerator OnFireRoutine()
+    {
+        for (float elapsed = 0; elapsed < 0.3f; elapsed += Time.deltaTime)
+        {
+            yield return null;
+            m_inner.color = Color.Lerp(Color.white, Color.green,
+                Mathf.PingPong(elapsed, elapsed * 0.5f));
+        }
+
+        m_inner.color = Color.white;
+    }
+}
--- Assets/Code/PlayerFireScript.cs
+++ Assets/Code/PlayerFireScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerFireScript : MonoBehaviour {
 
@@ -10,6 +11,13 @@ public class PlayerFireScript : MonoBehaviour {
     [SerializeField]
     PlayerBullet m_bulletPrefab;
 
+    [SerializeField]
+    Image m_beatRingImage;
+
+
+    [SerializeField]
+    Image m_beatRingInnerImage;
+    
     public enum BeatFireRating
     {
         Normal,
@@ -24,46 +32,82 @@ public class PlayerFireScript : MonoBehaviour {
     float m_perfectRatingSecondsBuffer = 0.2f;
 
     private float m_elapsedSecondsSinceLastBeat;
+    private bool m_alreadyFiredForThisBeat;
 
 	// Update is called once per frame
 	void Update ()
     {
         m_elapsedSecondsSinceLastBeat += Time.deltaTime;
         if (m_elapsedSecondsSinceLastBeat > m_secondsBetweenBeats)
-            m_elapsedSecondsSinceLastBeat = 0f;
-
-        if (Input.GetMouseButtonDown(0))
         {
+            m_alreadyFiredForThisBeat = false;
+            m_elapsedSecondsSinceLastBeat = 0;
+        }
+
+        UpdateBeatRing();
+
+        if (!m_alreadyFiredForThisBeat && Input.GetMouseButtonDown(0))
+        {
+            m_alreadyFiredForThisBeat = true;
             var bullet = Instantiate(m_bulletPrefab, transform.position, Quaternion.identity);
             bullet.Init(GetFireRating());
         }
+
 	}
 
     private float GetSecondsDifferenceFromBeat()
     {
-        return m_elapsedSecondsSinceLastBeat > m_goodRatingSecondsBuffer ?
-            m_secondsBetweenBeats - m_elapsedSecondsSinceLastBeat :
-            m_elapsedSecondsSinceLastBeat;
+        return Mathf.Abs((m_secondsBetweenBeats * 0.5f) - m_elapsedSecondsSinceLastBeat);
     }
 
     private BeatFireRating GetFireRating()
     {
+        // Use half the beat time as beat hit point and calculate difference from
+        // that midpoint as rating calculation. 
         var secondsDifferenceFromBeat = GetSecondsDifferenceFromBeat();
-        if (secondsDifferenceFromBeat < m_perfectRatingSecondsBuffer)
+        if (secondsDifferenceFromBeat < (m_perfectRatingSecondsBuffer * 0.5f))
             return BeatFireRating.Perfect;
-        else if (secondsDifferenceFromBeat < m_goodRatingSecondsBuffer)
+        else if (secondsDifferenceFromBeat < (m_goodRatingSecondsBuffer * 0.5f))
             return BeatFireRating.Good;
         else
             return BeatFireRating.Normal;
     }
-    /*
+
+    private void UpdateBeatRing()
+    {
+
+    }
+
+    private IEnumerator ShowInnerBeatRingFireROutine()
+    {
+        yield return null;
+    }
+
+    public float ToBeatCenterPercentage
+    {
+        get
+        {
+            return 1f - (GetSecondsDifferenceFromBeat() / (m_secondsBetweenBeats * 0.5f));
+        }
+    }
+
     void OnGUI()
     {
         GUI.TextArea(new Rect(0, 0, 400, 200),
-            string.Format("{0}\n{1}\n{2}",
+            string.Format("{0}\n{1}\n{2}\n{3}\n{4}",
             m_elapsedSecondsSinceLastBeat,
             GetSecondsDifferenceFromBeat(),
-            GetFireRating().ToString()));
+            GetFireRating().ToString(),
+            m_alreadyFiredForThisBeat,
+            true
+            ));
     }
-    */
+
+    private void OnValidate()
+    {
+        if (m_secondsBetweenBeats < m_goodRatingSecondsBuffer)
+            m_secondsBetweenBeats = m_goodRatingSecondsBuffer;
+        if (m_perfectRatingSecondsBuffer > m_goodRatingSecondsBuffer)
+            m_perfectRatingSecondsBuffer = m_goodRatingSecondsBuffer;
+    }
 }
