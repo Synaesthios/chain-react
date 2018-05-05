cda473e3cec1ed5bcef4ed974abe42a5269610f9
Zythiku <zythiku@gmail.com>
2018-05-05 12:53:44 -0700

    Add player bullet firing

---
+++ Assets/Code/PlayerBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Renderer))]
+public class PlayerBullet : MonoBehaviour {
+
+    [SerializeField]
+    private float m_fireForce = 2f;
+
+    private Renderer m_renderer;
+    private Rigidbody m_rigidBody;
+
+	public void Init (PlayerFireScript.BeatFireRating rating)
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_renderer = GetComponent<Renderer>();
+        m_rigidBody.AddForce(transform.forward, ForceMode.VelocityChange);
+
+        switch (rating)
+        {
+            case PlayerFireScript.BeatFireRating.Normal:
+                m_renderer.material.color = Color.white;
+                break;
+
+            case PlayerFireScript.BeatFireRating.Good:
+                m_renderer.material.color = Color.cyan;
+                break;
+            case PlayerFireScript.BeatFireRating.Perfect:
+                m_renderer.material.color = Color.green;
+                break;
+        }
+	}
+}
---
+++ Assets/Code/PlayerFireScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFireScript : MonoBehaviour {
+
+    [SerializeField]
+    float m_secondsBetweenBeats = 1f;
+
+    [SerializeField]
+    PlayerBullet m_bulletPrefab;
+
+    public enum BeatFireRating
+    {
+        Normal,
+        Good,
+        Perfect
+    }
+
+    [SerializeField]
+    float m_goodRatingSecondsBuffer = 0.3f;
+
+    [SerializeField]
+    float m_perfectRatingSecondsBuffer = 0.2f;
+
+    private float m_elapsedSecondsSinceLastBeat;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        m_elapsedSecondsSinceLastBeat += Time.deltaTime;
+        if (m_elapsedSecondsSinceLastBeat > m_secondsBetweenBeats)
+            m_elapsedSecondsSinceLastBeat = 0f;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var bullet = Instantiate(m_bulletPrefab, transform.position, Quaternion.identity);
+            bullet.Init(GetFireRating());
+        }
+	}
+
+    private float GetSecondsDifferenceFromBeat()
+    {
+        return m_elapsedSecondsSinceLastBeat > m_goodRatingSecondsBuffer ?
+            m_secondsBetweenBeats - m_elapsedSecondsSinceLastBeat :
+            m_elapsedSecondsSinceLastBeat;
+    }
+
+    private BeatFireRating GetFireRating()
+    {
+        var secondsDifferenceFromBeat = GetSecondsDifferenceFromBeat();
+        if (secondsDifferenceFromBeat < m_perfectRatingSecondsBuffer)
+            return BeatFireRating.Perfect;
+        else if (secondsDifferenceFromBeat < m_goodRatingSecondsBuffer)
+            return BeatFireRating.Good;
+        else
+            return BeatFireRating.Normal;
+    }
+    /*
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(0, 0, 400, 200),
+            string.Format("{0}\n{1}\n{2}",
+            m_elapsedSecondsSinceLastBeat,
+            GetSecondsDifferenceFromBeat(),
+            GetFireRating().ToString()));
+    }
+    */
+}
